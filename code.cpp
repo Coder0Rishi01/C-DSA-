9866b0361bcbd810b1c0fe9a296bac58ce7e9300
Rishikesh Pal <rishikeshpal937@gmail.com>
2025-10-21 10:02:53 +0530

    Lecture 17
    Rotated sorted Array

--- code.cpp
+++ code.cpp
@@ -2,52 +2,38 @@
 #include <vector>
 using namespace std;
 
-int binarySearch(vector<int> arr, int tar) {  // iterative 
-    int st = 0, end =arr.size()-1;
 
-    while(st <= end) {
-        // int mid = (st + end) /2;
-        int mid = st + (end - st)/2;
-         
-        if(tar > arr[mid]) {
-            st = mid + 1;
-        } else if (tar < arr[mid]) {
-            end = mid -1;
-        } else {
-            return mid;
+    int search(vector<int>& A, int tar) {
+        int st = 0, end = A.size()-1;
+
+        while(st <= end) {
+            int mid = st + (end-st)/2;
+
+            if(A[mid] == tar) {
+                return mid;
+            }
+            
+            if(A[st] <= A[mid]) { // Left  sorted
+                if(A[st] <= tar && tar <= A[mid]) {
+                    end = mid-1;
+                } else {
+                    st = mid + 1;
+                }
+            } else { // right sorted
+                if(A[mid] <= tar && tar <= A[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
         }
-    }
-
-    return -1;
-}
-
-int recBinarySearch(vector<int> arr, int tar, int st, int end) {
-    if(st < end) {
-        int mid = st + (end - st)/2;
-
-        if(tar > arr[mid]) { // 2nd half
-            return recBinarySearch(arr, tar,mid+1, end);
-        } else if (tar < arr[mid]) {  // 1st  half
-            return recBinarySearch(arr, tar, st, mid-1);
-        } else { // mid => ans
-            return mid;
-        }
-
-    }
         return -1;
-        
-}
+    }
 
 int main() {
-    vector<int> arr1 = {-1, 0,  3, 4, 5, 9, 12}; // odd
-    int tar1 = 4;
+    int arr = {3, 4, 5, 6, 7, 0, 1, 2} ;
+    int tar = 0;
 
-    //cout << binarySearch(arr1, tar1) << endl;
-
-    vector<int> arr2 = {-1, 0, 3, 5, 9, 12}; // even
-    int tar2 = 20;
-
-    cout << binarySearch(arr2, tar2) << endl;
-
+    cout << search
     return 0;
 }
