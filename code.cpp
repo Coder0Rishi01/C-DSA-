b871bea1215a236efdb578648a2283c0dc3bd195
Rishikesh Pal <rishikeshpal937@gmail.com>
2025-10-21 10:03:58 +0530

    Lecture 19

--- code.cpp
+++ code.cpp
@@ -2,23 +2,42 @@
 #include <vector>
 using namespace std;
 
- int peakIndexInMountainArray(vector<int>& A) {
-            int st = 1, end = A.size() - 2;
+                    // Single Element in a sorted Array 
+                    
 
-            while(st <= end) {
-                int mid = st + (end - st) / 2;
-                if(A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
-                    return mid;
-                } else if (A[mid - 1] < A[mid]) {
+ int singleNonDuplicate(vector<int>& A) {
+        int n =  A.size();
+        
+        if(n == 1) return A[0];
+
+        int st = 0, end = n-1;
+        while(st <= end) {
+            int mid = st + (end - st) /2;
+
+            if(mid == 0 && A[0] != A[1]) return A[mid];
+            if(mid == n-1 && A[n-1] != A[n-2]) return A[mid];
+
+            if(A[mid - 1] != A[mid] && A[mid] != A[mid + 1]) return A[mid];
+
+            if(mid % 2 == 0) { // even 
+                if(A[mid - 1] == A[mid]) { //left 
+                    end = mid - 1;
+                } else  { // right 
                     st = mid + 1;
-                } else {
+                }
+            } else { 
+                if(A[mid - 1] == A[mid]) { // right 
+                    st = mid + 1;
+                } else { // left 
                     end = mid - 1;
                 }
             }
-            return -1;
-    }
+        }
+        return -1;
+    };
 
-int main()  {
-    
-    return  0;
+
+int main() {
+
+    return 0;
 }
