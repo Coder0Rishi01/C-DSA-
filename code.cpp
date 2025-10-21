3e9bcd925c74fa201a3ab298351118afcbf6654f
Rishikesh Pal <rishikeshpal937@gmail.com>
2025-10-21 10:06:08 +0530

    Lecture 23

--- code.cpp
+++ code.cpp
@@ -1,51 +1,80 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-                                    // Aggressive Cows Problem 
+                            // Sorting Algorithm
 
+// 1. Bubble sort 
 
-bool isPossible(vector<int> &arr, int N, int C, int minAllowedDist) { // O(N)
-    int cows =  1, lastStallPos = arr[0];
+void bubbleSort(int arr[], int n) {
 
-    for(int i = 1; i<N; i++) {
-        if(arr[i] - lastStallPos >= minAllowedDist) {
-            cows++;
-            lastStallPos = arr[i];
-        }
-        if(cows == C) {
-            return true;
-        }
-    }
-    return false;
-}
+    for(int i=0; i<n-1; i++) {
+        bool isSwap = false;
 
-
-int getDistance(vector<int> &arr, int N, int C) {
-       sort(arr.begin(), arr.end()); // NlogN
-        // st = 1, end = maxVal - minVal;
-
-        int st = 1, end = arr[N-1] - arr[0], ans = -1;
-
-        while(st <= end) { // O(lon(Range) * N)
-            int mid = st + (end - st) /2;
-
-            if(isPossible (arr, N, C, mid)) { // right
-                ans = mid;
-                st = mid+1;
-            } else { // Left
-                end = mid - 1;
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
         }
-        return ans;
+        if(!isSwap) { // Array is already sorted  
+            return;
+        }
+    }
 }
 
-int main() {
-    int N = 5, C = 3;
-    vector<int> arr = {1, 2, 8, 4, 9};
+void  printArr(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    } 
+    cout << endl;
+}
 
-    cout << getDistance(arr, N, C) << endl;
+
+// 2. Selection sort 
+ 
+void selectionSort(int arr[], int n) {  // O(n^2)
+
+    for(int i=0; i<n-1; i++) {
+        int smallestIdx = i; // unsorted part starting 
+
+        for(int j = i+1; j<n; j++) {
+           if(arr[j] < arr[smallestIdx]) {
+            smallestIdx = j;
+           }
+        }
+
+        swap(arr[i], arr[smallestIdx]);
+    }
+}
+
+
+// 3. Insertion sort 
+
+void insertionSort(int arr[], int n) {  // O(n^2)
+
+    for(int i=1; i<n; i++) {
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev >= 0 && arr[prev] > curr) {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+
+        arr[prev + 1] = curr; // placing the curr el in its correct position
+    }
+}
+
+
+int main() {    
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+
+    //bubbleSort(arr, n);
+    //selectionSort(arr, n);
+    insertionSort(arr, n);
+    printArr(arr, n);
 
     return 0;
 }
