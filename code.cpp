add7ff566581d8ea1f5b20aa2afd7ac803906f72
Rishikesh Pal <rishikeshpal937@gmail.com>
2025-10-21 10:04:36 +0530

    Lecture 20

--- code.cpp
+++ code.cpp
@@ -1,43 +1,60 @@
 #include <iostream>
-#include <vector>
+#include <vector> 
 using namespace std;
 
-                    // Single Element in a sorted Array 
-                    
+                    // Book Allocation or Allocate books Problem
 
- int singleNonDuplicate(vector<int>& A) {
-        int n =  A.size();
-        
-        if(n == 1) return A[0];
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedpages) {
+    int student = 1, pages = 0;
 
-        int st = 0, end = n-1;
-        while(st <= end) {
-            int mid = st + (end - st) /2;
-
-            if(mid == 0 && A[0] != A[1]) return A[mid];
-            if(mid == n-1 && A[n-1] != A[n-2]) return A[mid];
-
-            if(A[mid - 1] != A[mid] && A[mid] != A[mid + 1]) return A[mid];
-
-            if(mid % 2 == 0) { // even 
-                if(A[mid - 1] == A[mid]) { //left 
-                    end = mid - 1;
-                } else  { // right 
-                    st = mid + 1;
-                }
-            } else { 
-                if(A[mid - 1] == A[mid]) { // right 
-                    st = mid + 1;
-                } else { // left 
-                    end = mid - 1;
-                }
-            }
+    for(int i=0; i<n; i++) {
+        if(arr[i] > maxAllowedpages) {
+            return false;
         }
+
+        if(pages + arr[i] <= maxAllowedpages) {
+            pages += arr[i];
+        } else {
+            student++;
+            pages = arr[i];
+        }
+    }
+
+    return student > m ? false : true;
+}
+
+int allocateBooks(vector<int> &arr, int n, int m) { // O(longN * n)
+    if(m > n) {
         return -1;
-    };
+    }
 
+    int sum = 0;
+    for(int i=0; i<n; i++) { // O(n)
+        sum += arr[i];
+    }
 
+    int ans = -1;
+    int st = 0, end = sum; // range of possible ans
+
+     while(st <= end) { // O(logN * n)
+        int mid = st + (end - st) /2;
+
+        if(isValid(arr,n,m, mid)) { // left
+            ans = mid;
+            end =  mid - 1;
+        } else { // right 
+            st = mid + 1;
+        }
+     }
+     return ans;
+}
+                    
 int main() {
+    // vector<int> arr =  {2, 1, 3, 4}; // 6
+    vector<int> arr = {15, 17, 20}; // 32
+    int n = 4, m = 2;
+
+    cout << allocateBooks(arr, n, m) << endl;
 
     return 0;
 }
